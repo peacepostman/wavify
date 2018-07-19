06339fe7449171a04c71f1f97ecf931b3ff4c46a
Joey Paris <joey@leadjig.com>
2018-07-19 14:14:56 -0400

    Adds warning to jQuery file if wavify is not a function. This is to help previous users immediately understand what change is required for this new arrangement

--- jquery.wavify.js
+++ jquery.wavify.js
@@ -5,8 +5,14 @@
  */
 (function ( $ ) {
 
-    $.fn.wavify = function( options ) {
-        return wavify( this, options );
-    };
+	$.fn.wavify = function( options ) {
+		if( 'function' !== typeof wavify )
+		{
+			console.error( "wavify is not a function. Be sure to include 'wavify.js' before you include 'jquery.wavify.js'." )
+			throw( "Error: wavify is not a function")
+		}
+
+		return wavify( this, options );
+	};
 
 }(jQuery));
